35d23914cce38ac9e083e68f774e16b42202d5c1
Taewoo Lee <lteawoo@naver.com>
2020-01-20 14:17:33 +0900

    이름 축소

--- src/main/java/kr/taeu/SpringSecurityPractice/Application.java
+++ src/main/java/kr/taeu/SpringSecurityPractice/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityPracticeApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityPracticeApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
