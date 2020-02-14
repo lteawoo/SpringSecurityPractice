878d075bfefc2529ad62643d453b4e4cfcab2556
Taewoo Lee <lteawoo@naver.com>
2020-02-14 19:50:06 +0900

    정비

--- src/main/java/kr/taeu/SpringSecurityPractice/global/security/service/CustomAuthenticationProvider.java
+++ src/main/java/kr/taeu/SpringSecurityPractice/global/security/service/CustomAuthenticationProvider.java
@@ -21,7 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class AuthenticationProviderImpl implements AuthenticationProvider{
+public class CustomAuthenticationProvider implements AuthenticationProvider{
 	private final UserDetailsService userDetailsServiceImpl;
 	private final PasswordEncoder passwordEncoder;
 	
